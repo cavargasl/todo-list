45fbcf458b5c615852247da511fecb07c00c502c
Camilo Vargas <camilovargasingm@gmail.com>
2024-05-24 13:55:53 -0500

    Add class-variance-authority dependency and update Button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,14 +1,39 @@
 import { cn } from "@/utils";
+import { cva, type VariantProps } from "class-variance-authority";
 
-type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+const buttonVariants = cva(
+  "inline-flex items-center justify-center rounded-lg font-semibold transition-all focus-visible:outline-none focus-visible:ring-foreground focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-50 ease-out-circ text-nowrap",
+  {
+    variants: {
+      variant: {
+        default:
+          "bg-background border-2 border-foreground ring-rose-500 hover:ring-2 gap-2",
+        ghost: "text-gray-500 hover:text-white hover:bg-foreground",
+      },
+      size: {
+        default: "h-[60px] p-3",
+        icon: "rounded px-1 py-2 h-[40px] w-[40px]",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
 
-export default function Button({ className, ...props }: ButtonProps) {
+type ButtonProps = VariantProps<typeof buttonVariants> &
+  React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+export default function Button({
+  className,
+  variant,
+  size,
+  ...props
+}: ButtonProps) {
   return (
     <button
-      className={cn(
-        "h-[60px] w-[350px] cursor-pointer rounded-lg bg-background border-2 border-foreground p-4 ring-rose-500 hover:ring-2 flex gap-2",
-        className
-      )}
+      className={cn(buttonVariants({ variant, size, className }))}
       {...props}
     />
   );
