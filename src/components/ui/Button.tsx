a1e904f7bf54ad3830abc0f25749aeacc1b9a83c
Camilo Vargas <camilovargasingm@gmail.com>
2024-05-24 12:56:26 -0500

    Refactor Button component to accept a className prop

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,7 +1,15 @@
-type ButtonProps = {
-  children: React.ReactNode
-}
+import { cn } from "@/utils";
 
-export default function Button({ children}: ButtonProps) {
-  return <button className="h-[60px] w-[350px] cursor-pointer rounded-lg bg-background border-2 border-foreground p-4 ring-rose-500 hover:ring-2">{children}</button>
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+export default function Button({ className, ...props }: ButtonProps) {
+  return (
+    <button
+      className={cn(
+        "h-[60px] w-[350px] cursor-pointer rounded-lg bg-background border-2 border-foreground p-4 ring-rose-500 hover:ring-2 flex gap-2",
+        className
+      )}
+      {...props}
+    />
+  );
 }
