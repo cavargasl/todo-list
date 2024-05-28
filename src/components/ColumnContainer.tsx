5b9c67a001b845ee4e537b63f3147d942545bc1b
Camilo Vargas <camilovargasingm@gmail.com>
2024-05-27 22:58:40 -0500

    chore: Update ColumnContainer footer with styled Button component

--- src/components/ColumnContainer.tsx
+++ src/components/ColumnContainer.tsx
@@ -8,16 +8,19 @@ type Props = {
 
 export default function ColumnContainer({ title }: Props) {
   return (
-    <div className="bg-foreground w-[350px] h-[500px] rounded-md flex flex-col">
-      <header className="text-lg bg-background h-[60px] rounded-t-md p-3 font-bold border-foreground border-4 flex justify-between items-center">
+    <div className="bg-foreground w-[350px] h-[500px] rounded-md flex flex-col p-1">
+      <header className="text-lg bg-background h-[60px] rounded-md p-3 font-bold flex justify-between items-center">
         {title}
         <Button variant={"ghost"} size={"icon"}>
           <TrashIcon />
         </Button>
       </header>
       <main className="flex flex-grow"></main>
-      <footer className="p-3 text-gray-500 flex gap-2">
-        <PlusIcon /> Add item
+      <footer>
+        <Button variant={"ghost"} size={"small"} className="w-full justify-start hover:bg-background">
+          <PlusIcon />
+          Add item
+        </Button>
       </footer>
     </div>
   );
