8b8b92e0f2b1590ec7e5267513d7d5f2bb7f9b87
Camilo Vargas <camilovargasingm@gmail.com>
2024-05-27 22:58:18 -0500

    chore: Update Button component styles to include gap between elements

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,16 +2,17 @@ import { cn } from "@/utils";
 import { cva, type VariantProps } from "class-variance-authority";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-lg font-semibold transition-all focus-visible:outline-none focus-visible:ring-foreground focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-50 ease-out-circ text-nowrap",
+  "inline-flex items-center justify-center rounded-lg font-semibold transition-all focus-visible:outline-none focus-visible:ring-foreground focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-50 ease-out-circ text-nowrap gap-2",
   {
     variants: {
       variant: {
         default:
-          "bg-background border-2 border-foreground ring-rose-500 hover:ring-2 gap-2",
+          "bg-background border-2 border-foreground ring-rose-500 hover:ring-2",
         ghost: "text-gray-500 hover:text-white hover:bg-foreground",
       },
       size: {
         default: "h-[60px] p-3",
+        small: "h-[40px] p-2",
         icon: "rounded px-1 py-2 h-[40px] w-[40px]",
       },
     },
