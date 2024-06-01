4ce2c1b6762a8ae413e92f415b30e035fe562dc0
Camilo Vargas <camilovargasingm@gmail.com>
2024-06-01 12:50:22 -0500

    chore: Update CheckBox component tests and add click event test

--- src/components/ui/__test__/CheckBox.test.tsx
+++ src/components/ui/__test__/CheckBox.test.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckBox from "../CheckBox";
 
 describe("CheckBox", () => {
@@ -9,14 +9,24 @@ describe("CheckBox", () => {
   });
 
   test("renders with custom className", () => {
-    const { container } = render(<CheckBox className="h-10 w-10 px-4 custom-class" />);
+    const { container } = render(
+      <CheckBox className="h-10 w-10 px-4 custom-class" />
+    );
     expect(container).toMatchSnapshot();
   });
 
   test("renders with additional props", () => {
     const { container } = render(
-      <CheckBox aria-label="checkbox" defaultChecked type="button"/>
+      <CheckBox aria-label="checkbox" defaultChecked disabled />
     );
     expect(container).toMatchSnapshot();
+  });
+
+  it("should change the checked state when clicked", () => {
+    render(<CheckBox />);
+    const checkbox = screen.getByRole("checkbox");
+    expect(checkbox).not.toBeChecked();
+    checkbox.click();
+    expect(checkbox).toBeChecked();
   });
 });
