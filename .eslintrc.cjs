d5e479b4c042b2485453e084cbfff53d0010d3f1
Camilo Vargas <camilovargasingm@gmail.com>
2024-05-31 23:46:30 -0500

    chore: Update eslint configuration and dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    "node_modules/standard/eslintrc.json"
+    "eslint-config-prettier"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
