eaec089fa40c983b58da051fb17140b05397b0c9
Camilo Vargas <camilovargasingm@gmail.com>
2024-06-01 11:31:41 -0500

    feat: Add unit tests for App component

---
+++ src/App.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+describe("App", () => {
+  it("renders app title", () => {
+    render(<App />);
+    const titleElement = screen.getByText(/Todo List/i);
+    expect(titleElement).toBeInTheDocument();
+  });
+
+  it("renders kanban board", () => {
+    render(<App />);
+    const boardElement = screen.getByRole("main", { name: /Kanban Board/i });
+    expect(boardElement).toBeInTheDocument();
+  });
+});
