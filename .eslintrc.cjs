2820697abcd040a024166294107ca79a91aa5716
Camilo Vargas <camilovargasingm@gmail.com>
2024-06-01 22:22:43 -0500

    chore: Update prettier and eslint configurations

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,29 @@
+/** @type {import("eslint").Linter.Config} */
+
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true},
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    "eslint-config-prettier"
+    "prettier",
+    "plugin:tailwindcss/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', 'node_modules'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', 'tailwindcss'],
+  plugins: ['react-refresh', '@typescript-eslint', 'tailwindcss'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
+    ],
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports',
+      },
     ],
   },
   settings: {
