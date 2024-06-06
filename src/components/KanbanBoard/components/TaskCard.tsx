f4df2341b99a2f41f904731a1bd434231abfae94
Camilo Vargas <camilovargasingm@gmail.com>
2024-06-06 01:42:49 -0500

    refactor: Update TaskCard component to use FunctionsTask type and fix aria-label

--- src/components/KanbanBoard/components/TaskCard.tsx
+++ src/components/KanbanBoard/components/TaskCard.tsx
@@ -1,4 +1,4 @@
-import { type Task } from "@/types"
+import { type FunctionsTask, type Task } from "@/types"
 
 import Button from "@/components/ui/Button"
 import CheckBox from "@/components/ui/CheckBox"
@@ -7,16 +7,18 @@ import ArrowRight from "@/components/icons/ArrowRight"
 import TrashIcon from "@/components/icons/TrashIcon"
 
 type TaskCardProps = {
-  Task: Task
+  task: Task
+  functionsTask: FunctionsTask
+  lastStage: boolean
 }
-export default function TaskCard({ Task: { name, completed } }: TaskCardProps) {
+export default function TaskCard({ task }: TaskCardProps) {
   return (
     <li
       aria-label="task"
       className="flex w-full gap-2 rounded-md bg-background p-2"
     >
-      <CheckBox className="peer" defaultChecked={completed} />
-      <p className="w-full peer-checked:line-through">{name}</p>
+      <CheckBox className="peer" defaultChecked={task.completed} />
+      <p className="w-full peer-checked:line-through">{task.name}</p>
       <div className="flex gap-1">
         <Button
           variant={"ghost"}
@@ -30,7 +32,7 @@ export default function TaskCard({ Task: { name, completed } }: TaskCardProps) {
           variant={"ghost"}
           size={"icon"}
           className="size-[20px] p-0"
-          arial-label="move to right"
+          aria-label="move to right"
         >
           <ArrowRight />
         </Button>
